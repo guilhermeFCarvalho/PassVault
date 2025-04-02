29df7c4513b049f4f84fbc7581dd8a88c8d6752a
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2025-04-02 11:03:26 -0300

    ref: remove unused model

--- app/src/main/java/com/example/passvault/features/password/domain/model/Vault.kt
+++
@@ -1,9 +0,0 @@
-package com.example.passvault.features.password.domain.model
-
-import androidx.room.Entity
-
-@Entity
-data class Vault(
-    val label: String,
-    val passwords: List<Password>
-)

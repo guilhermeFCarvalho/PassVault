0044e578e52158bc98d46273c3b47d1749b98891
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2025-06-04 23:41:42 -0300

    chore: add dokka to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.dokka)
 }
 
 android {
@@ -80,5 +81,6 @@ dependencies {
     implementation(libs.koin.androidx.compose.navigation)
     testImplementation(kotlin("test"))
     testImplementation(libs.kotlinx.coroutines.test)
+
 
 }
